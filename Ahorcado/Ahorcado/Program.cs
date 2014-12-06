91cf834fb0872769d45f78d5bea2096967a8928a
xOzone <julian.sanchez.1992@gmail.com>
2014-12-06 14:04:15 +0100

    Update to beta 0.0.5

--- Ahorcado/Ahorcado/Program.cs
+++ Ahorcado/Ahorcado/Program.cs
@@ -10,13 +10,93 @@ namespace Ahorcado
     {
         static void Main()
         {
-            Console.Title = "Ahorcado beta: v 0.0.2";
+            Console.Title = "Ahorcado beta: v 0.0.5";
             
             //Introducción de la versión beta...
             
             IntroBeta();
 
-            //Inicio del programa
+            //Introducción del programa. 
+
+            IntroPrograma();
+
+            //Menú del programa.
+
+            MenuPrograma();
+
+            //Opción seleccionada.
+
+            MenuOpcion();
+
+            Console.ReadKey();
+        }
+
+        private static void MenuOpcion()
+        {
+            string opcion;
+            int x;
+            bool sisepudo;
+            do
+            {
+                Console.Write("Opción seleccionada: ");
+                opcion = Console.ReadLine();
+                sisepudo = int.TryParse(opcion, out x);
+                if (!sisepudo)
+                    Console.WriteLine("La opción debe ser un número");
+            } while (!sisepudo);
+
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine("Case 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Case 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Case 3");
+                    break;
+                case 4:
+                    break;
+                default:
+                    Console.WriteLine("Las opciones van de la '1' a la '4'...Pulsa para volver al menú");
+                    Console.ReadKey();
+                    IntroPrograma();
+                    MenuPrograma();
+                    MenuOpcion();
+                    break;
+            }
+        }
+
+        private static void MenuPrograma()
+        {
+            Console.WriteLine("                                 *** MENU ***");
+            Console.WriteLine();
+            Console.WriteLine("                         1. Jugar (Beta). ");
+            Console.WriteLine("                         2. Jugar partida normal (En desarrollo)");
+            Console.WriteLine("                         3. Creditos.");
+            Console.WriteLine("                         4. Salir");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        private static void IntroPrograma()
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("    XXXXXXX  X     X  XXXXXXX   XXXXXXX   XXXXXXX  XXXXXXX  XXXX     XXXXXXX");
+            Console.WriteLine("    X     X  X     X  X     X   X     X   X        X     X  X   X    X     X");
+            Console.WriteLine("    X     X  X     X  X     X   X     X   X        X     X  X    X   X     X");
+            Console.WriteLine("    X     X  X     X  X     X   X     X   X        X     X  X     X  X     X");
+            Console.WriteLine("    XXXXXXX  XXXXXXX  X     X   X XXXXX   X        XXXXXXX  X     X  X     X");
+            Console.WriteLine("    X     X  X     X  X     X   X  X      X        X     X  X     X  X     X");
+            Console.WriteLine("    X     X  X     X  X     X   X   X     X        X     X  X    X   X     X");
+            Console.WriteLine("    X     X  X     X  X     X   X    X    X        X     X  X   X    X     X");
+            Console.WriteLine("    X     X  X     X  XXXXXXX   X     X   XXXXXXX  X     X  XXXX     XXXXXXX");
+            Console.WriteLine();
+            Console.WriteLine("                                                                    By Özone");
+            Console.WriteLine();
+            Console.WriteLine();
         }
 
         private static void IntroBeta()
