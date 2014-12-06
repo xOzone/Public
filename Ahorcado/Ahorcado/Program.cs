dfe8e16eb4dbe928e81e03184ffebef9cf98bab4
xOzone <julian.sanchez.1992@gmail.com>
2014-12-06 11:57:57 +0100

    Update 0.0.2

--- Ahorcado/Ahorcado/Program.cs
+++ Ahorcado/Ahorcado/Program.cs
@@ -8,8 +8,71 @@ namespace Ahorcado
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.Title = "Ahorcado beta: v 0.0.2";
+            
+            //Introducción de la versión beta...
+            
+            IntroBeta();
+
+            //Inicio del programa
+        }
+
+        private static void IntroBeta()
+        {
+            Console.WriteLine("Bienvenido al ahorcado");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("Presiona alguna tecla para continuar...");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Esta versión es una versión Beta, por lo que las funciones van a añadirse poco a poco para asegurar la estabilidad de la apicación");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Empecemos!");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("Presiona alguna tecla para continuar...");
+            Console.ReadKey();
         }
     }
 }
